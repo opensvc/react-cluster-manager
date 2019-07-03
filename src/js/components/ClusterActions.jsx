fef22f51059ddb68e51dc792d3b4c00ee7ee7edd
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-07-03 14:59:54 +0200

    Add action disabling to the ClusterActions component

--- src/js/components/ClusterActions.jsx
+++ src/js/components/ClusterActions.jsx
@@ -2,23 +2,88 @@ import React from "react";
 import { useStateValue } from '../state.js';
 import { apiNodeSetMonitor } from "../api.js";
 
-function ClusterActions(props) {
+function ClusterActionsSection(props) {
+	if (props.section == "divider") {
+		return (
+			<div className="dropdown-divider"></div>
+		)
+	}
+	return (
+		<div className={props.section["class"]}>
+			{props.section.actions.map((action, i) => (
+				<ClusterAction node={props.node} action={action} />
+			))}
+		</div>
+	)
+}
+
+function ClusterAction(props) {
 	const [{}, dispatch] = useStateValue();
 	function handleClick(e) {
 		var target = e.target.getAttribute("value")
 		apiNodeSetMonitor(target, (data) => dispatch({type: "parseApiResponse", data: data}))
 	}
+	if (props.action.disable) {
+		return (
+			<a href="#dummy" className="dropdown-item disabled" value={props.action.value}>{props.action.text}</a>
+		)
+	} else {
+		return (
+			<a href="#dummy" className="dropdown-item" value={props.action.value} onClick={handleClick}>{props.action.text}</a>
+		)
+	}
+}
+
+function ClusterActions(props) {
+	const [{cstat}, dispatch] = useStateValue();
+	const cdata = cstat.monitor.nodes
+	var actions = [
+		{
+			"section": "safe",
+			"class": "border-left-4 border-secondary",
+			"actions": [
+				{
+					"value": "frozen",
+					"text": "Freeze Nodes",
+					"disable": disable_freeze()
+				},
+				{
+					"value": "thawed",
+					"text": "Thaw Nodes",
+					"disable": disable_thaw()
+				}
+			]
+		},
+	]
+
+	function disable_freeze() {
+		for (var node in cdata) {
+			if (!cdata[node].frozen) {
+				return false
+			}
+		}
+		return true
+	}
+	function disable_thaw() {
+		for (var node in cdata) {
+			if (cdata[node].frozen) {
+				return false
+			}
+		}
+		return true
+	}
+
 	return (
-		<div className="dropdown">
+		<div className="dropdown position-static">
 			<button className="btn btn-sm btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cluster Actions</button>
 			<div className="dropdown-menu">
-				<a className="dropdown-item" value="frozen" onClick={handleClick}>Freeze Nodes</a>
-				<a className="dropdown-item" value="thawed" onClick={handleClick}>Thaw Nodes</a>
+				{actions.map((section, i) => (
+					<ClusterActionsSection key={i} node={props.node} path={props.path} section={section} />
+				))}
 			</div>
 		</div>
 	)
 }
-
 export {
 	ClusterActions
 }
