391b84f3a2843351b58a2d51cba54aa1aa0041be
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-07-09 18:58:36 +0200

    Add a grow spinner in ObjState
    Display it when the object status is not idle or when a
    global expect is set.

--- src/js/components/ObjState.jsx
+++ src/js/components/ObjState.jsx
@@ -5,6 +5,24 @@ import { ObjOverall } from "./ObjOverall.jsx";
 import { ObjFrozen } from "./ObjFrozen.jsx";
 import { ObjPlacement } from "./ObjPlacement.jsx";
 import { ObjProvisioned } from "./ObjProvisioned.jsx";
+import { Spinner } from 'reactstrap'
+
+function ObjActive(props) {
+	const [{ cstat }, dispatch] = useStateValue();
+	if (cstat.monitor === undefined) {
+		return null
+	}
+	for (var node in cstat.monitor.nodes) {
+		var idata = cstat.monitor.nodes[node].services.status[props.path]
+		if (idata === undefined) {
+			continue
+		}
+		if (idata.monitor.global_expect || (idata.monitor.status != "idle")) {
+			return ( <Spinner type="grow" color="primary" size="sm" /> )
+		}
+	}
+	return null
+}
 
 function ObjState(props) {
 	const [{ cstat }, dispatch] = useStateValue();
@@ -13,6 +31,7 @@ function ObjState(props) {
 	}
 	return (
 		<div>
+			<ObjActive path={props.path} />
 			<ObjOverall overall={cstat.monitor.services[props.path].overall} />
 			<ObjPlacement placement={cstat.monitor.services[props.path].placement} />
 			<ObjFrozen frozen={cstat.monitor.services[props.path].frozen} />
