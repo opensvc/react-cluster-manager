ecbd684d7be988521da66c29d7fd7b65f6d8f6c3
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-07-15 17:36:05 +0200

    Fix the scalers instance count

--- src/js/components/ObjInstanceCounts.jsx
+++ src/js/components/ObjInstanceCounts.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { useStateValue } from '../state.js';
 
+function slaveInstancesCount(path, cstat) {
+	var live = 0
+	for (var node in cstat.monitor.nodes) {
+		var nstat = cstat.monitor.nodes[node]
+		var instance = nstat.services.status[path]
+		if (!instance) {
+			continue
+		}
+		if (!instance.scaler_slave) {
+			continue
+		}
+		if (instance.avail == "up") {
+			live += 1
+		}
+	}
+	return live
+}
+
 function ObjInstanceCounts(props) {
 	const [{ cstat }, dispatch] = useStateValue();
 	if (cstat.monitor === undefined) {
@@ -9,20 +27,30 @@ function ObjInstanceCounts(props) {
 	var live = 0
 	var total = 0
 	var target = 0
-	var node
-	for (node in cstat.monitor.nodes) {
+	for (var node in cstat.monitor.nodes) {
 		var nstat = cstat.monitor.nodes[node]
 		var instance = nstat.services.status[props.path]
 		if (!instance) {
 			continue
+		}
+		if (instance.scaler_slave) {
+			continue
+		}
+
+		if ("scale" in instance) {
+			target = instance.scale
+			for (var slavePath of instance.scaler_slaves) {
+				live += slaveInstancesCount(slavePath, cstat)
+			}
+			return (
+				<span>{live}/{target}</span>
+			)
 		}
 		total += 1
 		if (instance.topology == "failover") {
 			target = 1
 		} else if (instance.topology == "flex") {
 			target = instance.flex_target
-		} else if (instance.topology == "scaler") {
-			target = instance.scale
 		}
 		if (instance.avail == "up") {
 			live += 1
