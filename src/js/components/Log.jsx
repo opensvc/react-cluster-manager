b4eac15d3bb7301f5a5c70e92146d22a6bd54e1f
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-07-16 09:38:49 +0200

    Implement logs filtering and log entry lookaround

--- src/js/components/Log.jsx
+++ src/js/components/Log.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Spinner } from "reactstrap"
 import { useLog } from "../hooks/Log.jsx"
+import { Input, FormGroup, Label } from 'reactstrap';
 
 function Log(props) {
 	const log = useLog(props.url)
+	const [search, setSearch] = useState("")
+	const [skip, setSkip] = useState()
+
 	if (props.noTitle) {
 		var title = null
 	} else {
 		var title = ( <h3>Logs</h3> )
 	}
+	function handleChange(e) {
+		setSearch(e.target.value)
+		location.href = "#"
+		setSkip(null)
+	}
+	useEffect(() => {
+		if (skip && (!location.href.match(RegExp("#"+skip+"$")))) {
+			location.href = "#"+skip
+		}
+	})
 	return (
 		<div className="pb-3">
 			{title}
-			<LogLines log={log} />
+			<FormGroup>
+				<Label for="search">Filter</Label>
+				<Input id="search" placeholder="RegExp" onChange={handleChange} value={search} />
+			</FormGroup>
+			<LogLines
+				log={log}
+				search={search}
+				setSearch={setSearch}
+				setSkip={setSkip}
+			/>
 		</div>
 	)
 }
@@ -21,10 +44,22 @@ function LogLines(props) {
 	if (!props.log) {
 		return ( <Spinner type="grow" color="primary" size="sm" /> )
 	}
+	if (props.search && (props.search.length>1)) {
+		var re = RegExp(props.search, "i")
+	} else {
+		var re
+	}
 	return (
 		<div className="d-flex flex-column-reverse text-break">
 			{props.log.map((line, i) => (
-				<LogLine key={i} data={line} />
+				<LogLine
+					key={i}
+					id={i}
+					data={line}
+					re={re}
+					setSearch={props.setSearch}
+					setSkip={props.setSkip}
+				/>
 			))}
 		</div>
 	)	
@@ -32,7 +67,7 @@ function LogLines(props) {
 
 function LogLine(props) {
 	var l = props.data.split(" - ")
-	var cl = "border-left-4 p-1 pl-2 "
+	var cl = "border-left-4 p-1 pl-2 clickable "
 	if (l[2] == "INFO") {
 	} else if (l[2] == "WARNING") {
 		cl += "border-warning"
@@ -41,8 +76,16 @@ function LogLine(props) {
 	} else if (l[2] == "DEBUG") {
 		cl += "border-muted"
 	}
+	if (props.re && !props.data.match(props.re)) {
+		return null
+	}
+
+	function handleClick(e) {
+		props.setSearch("")
+		props.setSkip(props.id)
+	}
 	return (
-		<div className={cl}>
+		<div className={cl} id={props.id} onClick={handleClick}>
 			<div className="text-secondary small">{l[0]} - {l[1]}</div>
 			<div>{l.slice(3).join(" - ")}</div>
 		</div>
