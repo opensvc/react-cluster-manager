eaef71ecb867551f1b14d91819caa6dbfcbbd5ef
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-07-30 16:45:06 +0200

    Remove unused import in NamespaceSelector

--- src/js/components/NamespaceSelector.jsx
+++ src/js/components/NamespaceSelector.jsx
@@ -9,7 +9,6 @@ import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 import Popper from '@material-ui/core/Popper';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
