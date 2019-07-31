a3ce2453529cc35d68d08f90cb3e12432728a319
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-07-31 13:41:10 +0200

    Make sure useObjConfig always return the latest version
    Cache by checksum.

--- src/js/hooks/ObjConfig.jsx
+++ src/js/hooks/ObjConfig.jsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { apiObjGetConfig } from "../api.js";
+import { useStateValue } from '../state.js';
 
 function useObjConfig(path) {
 	const [conf, setConf] = useState(null)
+	const [csum, setCsum] = useState(null)
+	const [{cstat}, dispatch] = useStateValue()
 
+	function getCsum() {
+		for (var node in cstat.monitor.nodes) {
+			if (path in cstat.monitor.nodes[node].services.config) {
+				return cstat.monitor.nodes[node].services.config[path].csum
+			}
+		}
+	}
 	function getConf() {
-		if (conf !== null) {
+		var liveCsum = getCsum()
+		if ((conf !== null) && (liveCsum == csum)) {
 			return
 		}
 		apiObjGetConfig({path: path}, (data) => {
+			console.log("load new", path, "config, csum", liveCsum)
+			setCsum(liveCsum)
 			setConf(data)
                 })
 	}
 
-	useEffect(() => {
-		getConf()
-	}, [])
-
+	getConf()
 	return conf
 }
 
