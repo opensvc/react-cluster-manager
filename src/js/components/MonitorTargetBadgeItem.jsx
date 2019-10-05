39256ffd42bbfe8b2951dfdff56232b31c9d56a2
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-10-05 11:58:43 +0200

    Add missing MonitorTargetBadgeItem file

---
+++ src/js/components/MonitorTargetBadgeItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useTranslation } from "react-i18next"
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import SkipNextIcon from "@material-ui/icons/SkipNext"
+
+function MonitorTargetBadgeItem(props) {
+	const { t, i18n } = useTranslation()
+	if (!props.target) {
+		return null
+	}
+        return (
+                <ListItem>
+                        <ListItemIcon>
+                                <SkipNextIcon color="primary" />
+                        </ListItemIcon>
+                        <ListItemText>
+                                {t("Target state is") + " " + props.target}
+                        </ListItemText>
+                </ListItem>
+        )
+}
+
+export {
+	MonitorTargetBadgeItem
+}
