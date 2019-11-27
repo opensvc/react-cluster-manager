450d9bff033a8f427997b6171992508a10ed77e7
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-11-27 17:56:58 +0100

    Fix the Namespaces not full width in the Cluster component

--- src/js/components/Cluster.jsx
+++ src/js/components/Cluster.jsx
@@ -8,16 +8,19 @@ const useStyles = makeStyles(theme => ({
         root: {
 		marginTop: theme.spacing(2),
         },
+        item: {
+		flexGrow: 1,
+        },
 }))
 
 function Cluster(props) {
 	const classes = useStyles()
 	return (
 		<Grid container spacing={2} className={classes.root}>
-			<Grid item md={6}>
+			<Grid item md={6} className={classes.item}>
 				<ClusterDigest />
 			</Grid>
-			<Grid item md={6}>
+			<Grid item md={6} className={classes.item}>
 				<SvcMap />
 			</Grid>
 		</Grid>
