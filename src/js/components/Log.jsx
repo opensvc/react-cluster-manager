7615c9bdceb53d9ec5bd136518875351cd22869b
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-12-23 11:59:00 +0100

    Log enhancements
    * Use the new structured log format from the agent api
    * Format metadata and allow filtering on the keys
    * Readability enhancements

--- src/js/components/Log.jsx
+++ src/js/components/Log.jsx
@@ -1,20 +1,24 @@
-import React, {useState, useEffect} from "react";
+import React, {Fragment, useState, useEffect} from "react"
 import { useLog } from "../hooks/Log.jsx"
-import { useTranslation } from 'react-i18next'
-import { TableToolbar } from "./TableToolbar.jsx";
+import { useTranslation } from "react-i18next"
+import { TableToolbar } from "./TableToolbar.jsx"
+import { stringToHslColor } from "../utils.js"
 
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Chip from '@material-ui/core/Chip'
+import Avatar from '@material-ui/core/Avatar'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
 import clsx from 'clsx'
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
-import Skeleton from '@material-ui/lab/Skeleton';
-import FilterListIcon from '@material-ui/icons/FilterList';
+import Tooltip from '@material-ui/core/Tooltip'
+import IconButton from '@material-ui/core/IconButton'
+import Skeleton from '@material-ui/lab/Skeleton'
+import FilterListIcon from '@material-ui/icons/FilterList'
+import ClearIcon from '@material-ui/icons/Clear'
 
 const useStyles = makeStyles(theme => ({
 	content: {
@@ -23,13 +27,24 @@ const useStyles = makeStyles(theme => ({
 	textField: {
 		width: "100%",
 	},
+	caption: {
+		marginLeft: -theme.spacing(1),
+	},
+	context: {
+		paddingBottom: theme.spacing(1),
+		paddingTop: theme.spacing(1),
+		width: "100%",
+		opacity: "0.7",
+	},
+	date: {
+		fontFamily: "monospace",
+		fontSize: "1em",
+		height: "1em",
+	},
 	logLine: {
 		width: "100%",
-		borderLeftWidth: "4px",
-		borderLeftStyle: "solid",
-		borderLeftColor: theme.palette.text.secondary,
-		paddingLeft: theme.spacing(1),
-		paddingBottom: theme.spacing(1),
+		wordBreak: "break-all",
+		fontFamily: "monospace",
 	},
 	log: {
 		display: "flex",
@@ -38,11 +53,14 @@ const useStyles = makeStyles(theme => ({
 		paddingTop: theme.spacing(2),
 	},
 	ERROR: {
-		borderColor: theme.status.danger,
+		color: theme.status.danger,
 	},
 	WARNING: {
-		borderColor: theme.status.warning,
+		color: theme.status.warning,
 	},
+	chip: {
+		border: "none",
+	}
 }))
 
 function Log(props) {
@@ -50,9 +68,16 @@ function Log(props) {
 	const [searchOpen, setSearchOpen] = useState(false)
 	const [search, setSearch] = useState("")
 	const [skip, setSkip] = useState()
+	const [context, setContext] = useState({})
 	const classes = useStyles()
 	const {t, i18n} = useTranslation()
 
+	function handleClear(e) {
+		setContext({})
+	}
+	function handleFilter(e) {
+		(!search) && setSearchOpen(!searchOpen)
+	}
 	function handleChange(e) {
 		setSearch(e.target.value)
 		location.href = "#"
@@ -70,8 +95,15 @@ function Log(props) {
                                 subheader={props.subheader}
 				action={
                                         <TableToolbar selected={[]} className={classes.table}>
+						{(Object.keys(context).length > 0) &&
+                                                <Tooltip title={t("Clear Filters")}>
+                                                        <IconButton aria-label="Filters" disabled={search?true:false} onClick={handleClear}>
+                                                                <ClearIcon />
+                                                        </IconButton>
+                                                </Tooltip>
+						}
                                                 <Tooltip title={t("Filters")}>
-                                                        <IconButton aria-label="Filters" disabled={search?true:false} onClick={() => {(!search) && setSearchOpen(!searchOpen)}}>
+                                                        <IconButton aria-label="Filters" disabled={search?true:false} onClick={handleFilter}>
                                                                 <FilterListIcon />
                                                         </IconButton>
                                                 </Tooltip>
@@ -91,11 +123,21 @@ function Log(props) {
 					value={search}
 				/>
 				}
+				<PositiveContext
+					setContext={setContext}
+					context={context}
+				/>
+				<NegativeContext
+					setContext={setContext}
+					context={context}
+				/>
 				<LogLines
 					log={log}
 					search={search}
 					setSearch={setSearch}
 					setSkip={setSkip}
+					setContext={setContext}
+					context={context}
 				/>
 			</CardContent>
                 </Card>
@@ -104,52 +146,235 @@ function Log(props) {
 }
 
 function LogLines(props) {
+	const { log, search, setSearch, setSkip, setContext, context } = props
 	const classes = useStyles()
-	if (!props.log) {
+	if (!log) {
 		return ( <CircularProgress color="primary" /> )
 	}
-	if (props.search && (props.search.length>1)) {
-		var re = RegExp(props.search, "i")
-	} else {
-		var re
+	var re
+	if (search && (search.length>1)) {
+		try {
+			re = RegExp(search, "i")
+		} catch(e) {}
 	}
 	return (
 		<div className={classes.log}>
-			{props.log.map((line, i) => (
+			{log.map((line, i) => (
 				<LogLine
 					key={i}
 					id={i}
 					data={line}
+				        prev={i ? log[i-1] : null}
 					re={re}
-					setSearch={props.setSearch}
-					setSkip={props.setSkip}
+					setSearch={setSearch}
+					setSkip={setSkip}
+					setContext={setContext}
+					context={context}
 				/>
 			))}
 		</div>
 	)	
 }
 
-function LogLine(props) {
+function LogLineContextKey(props) {
+	const { k, v, context, setContext, dense } = props
 	const classes = useStyles()
-	if (!props.data) {
+	if (!k || !v) {
+		return null
+	}
+	function dropContextKey() {
+		var newContext = {...context}
+		delete newContext[k]
+		setContext(newContext)
+	}
+	function negateContextKey() {
+		var newContext = {...context}
+		newContext[k] = {"value": v, "negate": true}
+		setContext(newContext)
+	}
+	function setPositiveContextKey() {
+		var newContext = {...context}
+		newContext[k] = {"value": v}
+		setContext(newContext)
+	}
+	function handleClick(e) {
+		if (k in context) {
+			if (context[k].negate) {
+				if (context[k].value == v) {
+					dropContextKey()
+				} else {
+					setPositiveContextKey()
+				}
+			} else {
+				negateContextKey()
+			}
+		} else {
+			setPositiveContextKey()
+		}
+	}
+	if ((k in context) && !context[k].negate) {
+		var color = "primary"
+	} else {
+		var color = "default"
+	}
+	if (dense && (k == "sid")) {
+		var label = ""
+	} else {
+		var label = v
+	}
+	return (
+		<Chip
+			className={classes.chip}
+			variant="outlined"
+			size="small"
+			color={color}
+			avatar={<Avatar style={{"backgroundColor": stringToHslColor(v, 30, 80)}}>{k}</Avatar>}
+			label={label}
+			onClick={handleClick}
+		/>
+	)
+}
+
+function PositiveContext(props) {
+	const { context, setContext } = props
+	const { t, i18n } = useTranslation()
+	var positiveContext = {}
+	for (var k in context) {
+		if (!context[k].negate) {
+			positiveContext[k] = context[k]
+		}
+	}
+	if (Object.keys(positiveContext).length == 0) {
+		return null
+	}
+	return (
+		<Fragment>
+			<Typography>
+				{t("With")}
+			</Typography>
+			&nbsp;
+			{Object.keys(positiveContext).map((k) => (
+				<LogLineContextKey
+					key={k}
+					k={k}
+					v={context[k].value}
+					context={context}
+					setContext={setContext}
+					dense={false}
+				/>
+			))}
+		</Fragment>
+	)
+}
+
+function NegativeContext(props) {
+	const { context, setContext } = props
+	const { t, i18n } = useTranslation()
+	var negativeContext = {}
+	for (var k in context) {
+		if (context[k].negate) {
+			negativeContext[k] = context[k]
+		}
+	}
+	if (Object.keys(negativeContext).length == 0) {
+		return null
+	}
+	return (
+		<Fragment>
+			<Typography>
+				{t("Excluding")}
+			</Typography>
+			&nbsp;
+			{Object.keys(negativeContext).map((k) => (
+				<LogLineContextKey
+					key={k}
+					k={k}
+					v={context[k].value}
+					context={context}
+					setContext={setContext}
+					dense={false}
+				/>
+			))}
+		</Fragment>
+	)
+}
+
+function LogLineContext(props) {
+	const { data, context, setContext } = props
+	const classes = useStyles()
+	if (Object.keys(data).length == 0) {
+		return null
+	}
+	return (
+		<div className={classes.context}>
+			{Object.keys(data).map((k) => (
+				<LogLineContextKey
+					key={k}
+					k={k}
+					v={data[k]}
+					context={context}
+					setContext={setContext}
+					dense={true}
+				/>
+			))}
+		</div>
+	)
+}
+
+function LogLine(props) {
+	const { re, data, prev, context, setContext, setSearch, setSkip, id } = props
+	const classes = useStyles()
+	if (!data) {
 		return <Skeleton />
 	}
-	var l = props.data.split(" - ")
-	var level = l[2]
-	if (props.re && !props.data.match(props.re)) {
+	var level = data.l
+	var msg = data.m.join("\n")
+	var date = new Date(data.t*1000)
+	for (var k in context) {
+		var v = context[k]
+		if (v.negate) {
+			if (data.x[k] == v.value) {
+				return null
+			}
+		} else {
+			if (data.x[k] != v.value) {
+				return null
+			}
+		}
+	}
+	if (re && !msg.match(re)) {
 		return null
 	}
 	function handleClick(e) {
-		props.setSearch("")
-		props.setSkip(props.id)
+		setSearch("")
+		setSkip(id)
 	}
 	return (
-		<div className={clsx(classes.logLine, classes[level])} id={props.id} onClick={handleClick}>
-			<Typography variant="caption" color="textSecondary" display="block">
-				{l[0]} - {l[1]}
-			</Typography>
-			{l.slice(3).join(" - ")}
-		</div>
+		<Fragment>
+			{(!prev || (JSON.stringify(prev.x) != JSON.stringify(data.x))) &&
+			<LogLineContext
+				data={data.x}
+				setContext={setContext}
+				context={context}
+			/>
+			}
+			<div className={classes.logLine} id={id}>
+				<div className={classes[level]}>
+					<Chip
+						className={clsx(classes.chip, classes.caption, classes.date)}
+						color="primary"
+						variant="outlined"
+						size="small"
+						//avatar={<Avatar>t</Avatar>}
+						label={date.toLocaleString()}
+						onClick={handleClick}
+					/>
+					{data.m.map((line, i) => (
+						<span key={i}>{line}</span>
+					))}
+				</div>
+			</div>
+		</Fragment>
 	)
 }
 
