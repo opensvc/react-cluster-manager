76a2728843d2319726bf202fe766c6940a0ff3b8
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-03-12 09:58:37 +0100

    Use a material Tooltip for the leader icon

--- src/js/components/ObjLeader.jsx
+++ src/js/components/ObjLeader.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React from "react"
+import { useTranslation } from "react-i18next"
 
-import { useColorStyles } from '../styles.js'
-import Typography from '@material-ui/core/Typography';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { useColorStyles } from "../styles.js"
+import Typography from "@material-ui/core/Typography"
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp"
+import Tooltip from "@material-ui/core/Tooltip"
 
 function ObjLeader(props) {
 	const classes = useColorStyles()
@@ -10,11 +12,11 @@ function ObjLeader(props) {
 		return null
 	}
 	return (
-		<span title="leader">
+		<Tooltip title="Leader">
 			<Typography component="span" color="primary">
 				<KeyboardArrowUpIcon />
 			</Typography>
-		</span>
+		</Tooltip>
 	)
 }
 
