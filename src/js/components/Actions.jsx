719f93547f4b2b24f4cd4dcea53d077597bd7769
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-03-12 09:07:24 +0100

    Add a tooltip to the "Actions" IconButton

--- src/js/components/Actions.jsx
+++ src/js/components/Actions.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from "react";
-import { useStateValue } from '../state.js';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Divider from '@material-ui/core/Divider';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
-import Dialog from '@material-ui/core/Dialog';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import Fab from '@material-ui/core/Fab';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import React, { useState } from "react"
+import { useTranslation } from "react-i18next"
+import { useStateValue } from "../state.js"
+import { makeStyles } from "@material-ui/core/styles"
+import IconButton from "@material-ui/core/IconButton"
+import Button from "@material-ui/core/Button"
+import Menu from "@material-ui/core/Menu"
+import MenuItem from "@material-ui/core/MenuItem"
+import Divider from "@material-ui/core/Divider"
+import LinearProgress from "@material-ui/core/LinearProgress"
+import DialogTitle from "@material-ui/core/DialogTitle"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogActions from "@material-ui/core/DialogActions"
+import Dialog from "@material-ui/core/Dialog"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import Checkbox from "@material-ui/core/Checkbox"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import FormControl from "@material-ui/core/FormControl"
+import FormGroup from "@material-ui/core/FormGroup"
+import Fab from "@material-ui/core/Fab"
+import PlayArrowIcon from "@material-ui/icons/PlayArrow"
+import Tooltip from "@material-ui/core/Tooltip"
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -135,6 +137,7 @@ function ConfirmationDialog(props) {
 function Actions(props) {
 	const [open, setOpen] = React.useState(false)
 	const classes = useStyles()
+	const { t } = useTranslation()
 
 	function handleClickOpen(e) {
 		e.stopPropagation()
@@ -178,7 +181,9 @@ function Actions(props) {
 
 	return (
 		<React.Fragment>
-			{button}
+			<Tooltip title={t("Actions")}>
+				{button}
+			</Tooltip>
 			<ConfirmationDialog
 				submit={props.submit}
 				path={props.path}
