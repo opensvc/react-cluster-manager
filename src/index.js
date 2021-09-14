d881e1f93ef19c5890e3abc6401a332bb2e54bf7
Cyril Galibern <cyril.galibern@opensvc.com>
2021-09-14 14:16:59 +0200

    log app app version during startup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import 'typeface-roboto';
 import "./index.css";
 import './i18n.js';
 import App from "./js/components/App.jsx";
+import {version} from "./version"
 
+console.log("running app version ", version)
